49a1aefa71e5a50532672959265f2be3dea7bec6
zaabed <zaaabedkhaan@gmail.com>
2023-06-21 01:40:21 +0600

    load id

--- index.js
+++ index.js
@@ -3,12 +3,28 @@ const app = express();
 const cors = require('cors');
 const port = process.env.PORT || 5000;
 
-// const chefDetails = require('./data/chefDetails.json')
+const chefDetails = require('./data/chefDetails.json')
 
 app.use(cors());
 
 app.get('/', (req, res) => {
     res.send('Chef Recipe is Running ');
+})
+
+app.get('/chefDetails', (req, res) => {
+    res.send(chefDetails);
+})
+
+app.get('/chefDetails/:id', (req, res) => {
+    const id = parseInt(req.params.id);
+    if (id === 0) {
+        res.send(chefDetails);
+    }
+    else {
+        const selectedChef = chefDetails.find(c => parseInt(c._id) === id);
+        res.send(selectedChef);
+    }
+
 })
 
 app.listen(port, () => {
